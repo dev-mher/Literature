036bea2b1db478ff545be6cbc94f6eda4d203a38
tux-dev <www.mher777@mail.ru>
2017-06-06 21:59:19 +0400

    Integrated 'Crashlytics'
    - Created a new account in fabric.io and added the app to track in dashboard.
    - Set up the required settings for the 'Crashlytics'.
    - Prevented the crash tracking for the 'debug' mode.

--- Literature/app/src/main/java/com/literature/android/literature/MyApplication.java
+++ Literature/app/src/main/java/com/literature/android/literature/MyApplication.java
@@ -3,6 +3,9 @@ package com.literature.android.literature;
 import android.app.Application;
 import android.content.SharedPreferences;
 
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by MherDavid on 28.05.2017.
  */
@@ -17,6 +20,9 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (!BuildConfig.DEBUG) {
+            Fabric.with(this, new Crashlytics());
+        }
         SharedPreferences prefs = getSharedPreferences(RATE_US_PREFS, MODE_PRIVATE);
         prefs.edit().putLong(FIRST_LAUNCH_KEY, System.currentTimeMillis()).apply();
         prefs.edit().putInt(DAYS_COUNT_KEY, 2).apply();
